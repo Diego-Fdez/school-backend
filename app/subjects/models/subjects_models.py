a5266f443ef91507c9e61b2d661bd80ce9522a6c
Diego-Fdez <dieghoxcr7@gmail.com>
2023-06-26 11:20:43 -0600

    create subjects models

---
+++ app/subjects/models/subjects_models.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+import uuid
+
+# create a pydantic model for subjects
+class Subject_Create(BaseModel):
+    name: str
+    user_id: uuid.UUID
+
+# create a pydantic model for subjects responses
+
+class Subject_Response(Subject_Create):
+    id: uuid.UUID
+    class Config:
+        orm_mode = True
