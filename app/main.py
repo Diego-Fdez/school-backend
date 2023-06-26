90786fc67f5ed82b83d605a3edbbf71518c27d68
Diego-Fdez <dieghoxcr7@gmail.com>
2023-06-26 11:20:00 -0600

    add subjects routes

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from .institutions.routes import institution_routes
 from .auth.routes import auth_routes
 from .students.routes import students_routes
 from .levels.routes import levels_routes
+from .subjects.routes import subjects_routes
 
 # Create an instance of the FastAPI class
 app = FastAPI(
@@ -19,6 +20,7 @@ app.include_router(institution_routes.router)
 app.include_router(auth_routes.router)
 app.include_router(students_routes.router)
 app.include_router(levels_routes.router)
+app.include_router(subjects_routes.router)
 
 # Define the root route
 @app.get("/")
